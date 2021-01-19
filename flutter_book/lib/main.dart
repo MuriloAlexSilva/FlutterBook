2cead75d2314cf74f3e048ccd766a4f6f32943ec
Murilo Silva <murilo.alex.silva@gmail.com>
2021-01-19 09:16:03 -0300

    .

--- flutter_book/lib/main.dart
+++ flutter_book/lib/main.dart
@@ -1,11 +1,7 @@
 import 'dart:io';
+
 import 'utils.dart' as utils;
 import 'package:flutter/material.dart';
-import 'package:path_provider/path_provider.dart';
-import 'pages/appointments.dart';
-import 'pages/contacts.dart';
-import 'pages/notes.dart';
-import 'pages/tasks.dart';
 
 void main() {
   startMeUp() async {
@@ -20,29 +16,6 @@ void main() {
 class FlutterBook extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: DefaultTabController(
-        //Para controlar o tabController
-        length: 4,
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text("Flutter Book"),
-            bottom: TabBar(
-              tabs: [
-                Tab(icon: Icon(Icons.date_range), text: "Appointments"),
-                Tab(icon: Icon(Icons.contacts), text: "Appointments"),
-                Tab(icon: Icon(Icons.note), text: "Appointments"),
-                Tab(
-                    icon: Icon(Icons.assignment_turned_in),
-                    text: "Appointments")
-              ],
-            ),
-          ),
-          body: TabBarView(
-            children: [Appointments(), Contacts(), Notes(), Tasks()],
-          ),
-        ),
-      ),
-    );
+    return Container();
   }
 }
